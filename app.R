603e47191668493f1f10c74dbe25aaa3d947500e
dhyoon1112 <dhyoon1112@gmail.com>
2021-12-22 22:52:23 -0600

    Update app.R
    Removed overwritten code (commented out before)

--- app.R
+++ app.R
@@ -1,4 +1,3 @@
-
 library(shiny)
 library(shinydashboard)
 library(stringi)
@@ -8,21 +7,7 @@ library(gsubfn)
 library(RWeka)
 library(ggplot2)
 
-setwd("C:/Users/dyoon/Desktop/Me/Projects/R/R Coursera/Capstone/final/en_US")
 file <- readRDS(file="./cleanfile")
-
-# blogs <- "./en_US.blogs.txt"
-# news <- "./en_US.news.txt"
-# twitter <- "./en_US.twitter.txt"
-# 
-# #analysis of files
-# blogs_file <- readLines(blogs)
-# news_file <- readLines(news)
-# twitter_file <- readLines(twitter)
-# 
-# bnt_file <- c(blogs_file, news_file, twitter_file)
-# Encoding(bnt_file) <- "UTF-8"
-# bnt_file <- tolower(bnt_file)
 
 list_vectors <- function(x,y) {
     
@@ -45,15 +30,9 @@ list_vectors <- function(x,y) {
         #Substring the vector, for anything that starts with argument x 
         list[i] <- substr(bnt_file[i], str_start, str_end)
     }
-    
-    # string_list <- str_replace_all(bnt_file, "[^[:alnum:]]", " ")
-    # string_list <- stripWhitespace(bnt_file)
-    # string_list <- removePunctuation(bnt_file)
-    # string_list <- removeNumbers(bnt_file)
 
     # #Retrieve the top 10 n-grams with the predicted next word 
     corpus <- VCorpus(VectorSource(bnt_file))
-    
     Tokenizer <- function(x) NGramTokenizer(x, Weka_control(min = y[1] + 1, max = y[1] + 1))
     Matrix <- TermDocumentMatrix(corpus, control = list(tokenize = Tokenizer))
     Corpus <- findFreqTerms(Matrix, lowfreq = 10)
@@ -63,41 +42,12 @@ list_vectors <- function(x,y) {
     CorpusCount <- data.frame(text = names(CorpusCount), frequency = CorpusCount)
     predictedword <- " "
     CorpusCount <- cbind.data.frame(CorpusCount,predictedword)
-    
     for (i in 1:nrow(CorpusCount)){
         CorpusCount[i,3] <- tail(strsplit(CorpusCount[i,1],split=" ")[[1]],1)
-    
+        
     return(CorpusCount)
     }
 }
-
-
-# # Define UI for application that draws a histogram
-# ui <- fluidPage(
-# 
-#     # Application title
-#     titlePanel("What is the Next Word?"),
-#     
-#     # Sidebar with a text input
-#     sidebarLayout(
-#         sidebarPanel(
-#             
-#             tags
-#             textInput("caption", "Type in a phrase:", value = "can you believe"),
-# 
-#         ),
-#         
-#      # Show a plot of the generated distribution
-#     mainPanel(
-#         textOutput("value1"),
-#         textOutput("value2"),
-#         textOutput("value3"),
-#         textOutput("value4")
-#         
-#         
-#         )
-#     )
-# )
 
 # Define UI for application that draws a histogram
 
@@ -118,11 +68,7 @@ body <- dashboardBody(
     textOutput("inputvalue"),
     h2("and your possible phrases with the predicted words are... "),
     h3("Please allow a minute for the algorithm to run."),
-    textOutput("value1"),
-    # textOutput("value2"),
-    # textOutput("value3"),
-    # textOutput("value4")
-    
+    textOutput("value1")    
 )
 
 ui <- dashboardPage(header,
@@ -130,15 +76,10 @@ ui <- dashboardPage(header,
                     body
 )
 
-
-
 # Define server logic required to draw a histogram
 server <- function(input, output) {
     
-    #observeEvent(input$caption, {
-        #session$sendCustomMessage(type = 'testmessage', message = 'Thank you for clicking')
     observe({
-        
         #argument for shiny app later on
         x_length <- sapply(strsplit(input$caption," "), length)
         print("breakpoint1")
@@ -153,16 +94,6 @@ server <- function(input, output) {
         output$value1 <- renderText({
             toString(string_list()[1,1])
         })
-        # output$value2 <- renderText({
-        #     toString(string_list()[2,1])
-        # })
-        # output$value3 <- renderText({
-        #     toString(string_list()[3,1])
-        # })
-        # output$value4 <- renderText({
-        #     toString(string_list()[4,1])
-        # })
-        
     })
 }
 
